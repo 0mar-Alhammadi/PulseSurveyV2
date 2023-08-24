7cff60d2c38f51f44d0f0c67b1da8f422f190be7
omar.alhammadi <omar.alhammadi@talabat.com>
2023-08-24 17:34:41 +0400

    added unit testing for one controller?

--- Models/UnifiedContext.cs
+++ Models/UnifiedContext.cs
@@ -14,6 +14,6 @@ public class UnifiedContext : DbContext
     public DbSet<User> Users { get; set; } = null!;
 }
 
-// dotnet aspnet-codegenerator controller -name AnswersController -async -api -m AnswerDTO -dc UnifiedContext -outDir Controllers
-// dotnet aspnet-codegenerator controller -name SurveysController -async -api -m Survey -dc UnifiedContext -outDir Controllers
-// dotnet aspnet-codegenerator controller -name UsersController -async -api -m User -dc UnifiedContext -outDir Controllers
+// todo
+// unit tests
+// docker
